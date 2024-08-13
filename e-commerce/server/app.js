61793a4d10d8a55cd63b29aad1f3a42630cff66d
Samuel Le <robbo_rules1@live.com.au>
2024-08-13 20:02:33 +1000

    Connect database with express

--- e-commerce/server/app.js
+++ e-commerce/server/app.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const db = require('./db');
+
 const app = express();
 const PORT = 3000;
+
+
+app.get('/', async (req, res) => {
+    try {
+      const result = await db.query('SELECT * FROM "Products"');
+      res.json(result.rows);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    }
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
