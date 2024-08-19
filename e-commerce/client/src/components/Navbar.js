a8b6561dd457eea74990d46762cd286665eb140c
Samuel Le <robbo_rules1@live.com.au>
2024-08-19 17:54:06 +1000

    Added navbar component in root

---
+++ e-commerce/client/src/components/Navbar.js
@@ -0,0 +1,21 @@
+
+import React from "react";
+
+
+
+export default function Navbar()
+{
+    return (
+        <div className="w-full h-14 flex flex-row text-white bg-cyan-600">
+            <p className="mt-3 ml-10" >Games Logo</p>
+            <ul className="mt-3 ml-auto flex flex-row gap-2">
+                <a href="/home" className="hover:underline">Home</a>
+                <a className="hover:underline">test</a>
+            </ul>
+            <div className="mt-3 ml-auto mr-10 flex flex-row gap-2">
+                <a href="/sign-in" className="hover:underline">Sign In</a>
+                <a href="/cart" className="hover:underline">Cart</a>
+            </div>
+        </div>
+    )
+}
