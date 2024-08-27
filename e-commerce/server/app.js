6e3a7056a0ab514dfef42bd4e88489e0b84bef57
Samuel Le <robbo_rules1@live.com.au>
2024-08-27 16:18:37 +1000

    Install and setup cors

--- e-commerce/server/app.js
+++ e-commerce/server/app.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+
 const db = require('./db');
 
 const app = express();
-const PORT = 3000;
+const PORT = 2000;
 
+app.use(cors());
 
-app.get('/', async (req, res) => {
+app.get('/game', async (req, res) => {
     try {
       const result = await db.query('SELECT * FROM products');
-      res.json(result.rows);
+      res.send(result);
     } catch (err) {
       console.error(err);
       res.status(500).send('Internal Server Error');
